f85ba18f27d03d73932d0880d0eca1352edab6d9
jimmyMac <jimmyli0919@163.com>
2019-05-10 19:52:18 +0800

    update

--- 技术学习/CV-learning/code/scipy-demo1.py
+++ 技术学习/CV-learning/code/scipy-demo1.py
@@ -1,6 +1,9 @@
 from PIL import Image
+from pylab import *
 from numpy import *
 from scipy.ndimage import filters
 
 im = array(Image.open('../images/test.jpg').convert('L'))
-im2 = filters.gaussian_filter(im,5)
+im2 = filters.gaussian_filter(im,15)
+imshow(im2)
+show()
