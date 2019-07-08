419cd593cbe7d85ccc7b990a32f3cfd2fa0784db
Li Jimmy <351793645@qq.com>
2019-07-08 10:14:33 +0800

    up

--- 技术学习/CV-learning/code/SciPy-demo1.py
+++
@@ -1,7 +0,0 @@
-#_*_coding:utf-8_*_
-from PIL import Image
-from numpy import *
-from scipy.ndimage import filters
-
-im = array(Image.open('../images/test.jpg').convert('L'))
-im2 = filters.gaussian_filter(im,5)
